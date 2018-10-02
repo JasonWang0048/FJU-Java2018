358621a3702e5c277abbcf62be2d3fc1004107cd
Jason <user@es302n31>
2018-10-02 10:51:10 +0800

    variable

--- myproject/src/com/jason/Hello.java
+++ myproject/src/com/jason/Hello.java
@@ -5,6 +5,17 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		int age = 19;
+		String name = "Jason";
+		float height = 169.5f;
+		float weight = 49.5f;
+		int schoolYear = 107;
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(height);
+		System.out.println(weight);
+		System.out.println(schoolYear);
 	}
 
 }
