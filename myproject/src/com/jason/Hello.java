47e5600b8a49d27fc41b6e423bf34619fb144fd6
Jason <user@es302n05>
2018-10-09 12:03:14 +0800

    Constructor

--- myproject/src/com/jason/Hello.java
+++ myproject/src/com/jason/Hello.java
@@ -4,7 +4,17 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		Person  p = new Person(49.5f, 1.7f);
+//		p.weight = 49.5f;
+//		p.height = 1.7f;
+		System.out.println(p.bmi());
+		p.hello();
+//		Person jason = new Person();
+//		Person jack = null;
+//		jack.hello();
+		
+		
+		/*System.out.println("Hello World!");
 		int age = 19;
 		String name = "Jason";
 		float height = 169.5f;
@@ -15,7 +25,7 @@ public class Hello {
 		System.out.println(age);
 		System.out.println(height);
 		System.out.println(weight);
-		System.out.println(schoolYear);
+		System.out.println(schoolYear);*/
 	}
 
 }
